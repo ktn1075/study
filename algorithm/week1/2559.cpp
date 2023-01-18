030a1304a096c19acde7f92b2c12c27c9b3ff084
ktn1114 <ktn1075@a.ut.ac.kr>
2023-01-19 00:11:41 +0900

    2559 문제 풀이

--- algorithm/week1/2559.cpp
+++ algorithm/week1/2559.cpp
@@ -1,10 +1,29 @@
 #include<iostream>
+#include<vector>
 
 
 using namespace std;
 int main(void)
 {
 
+	int maxCount, rotation;
+	int tempList[100200] = { 0 };
+;	int max = -991199;
+	cin >> maxCount >> rotation;
 
-
+	for (int i = 1; i <=maxCount; i++)
+	{
+		int temp = 0;
+		cin >> temp;
+		tempList[i] = tempList[i - 1] + temp;
+	}
+	for (int j = rotation; j <=maxCount; j++)
+	{
+		int x = tempList[j] - tempList[j-rotation];
+		if (max < x)
+		{
+			max = x;
+		}
+	}
+	cout << max;
 }
