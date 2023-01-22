c945c201c29fc3a93bc0a724ea7362a6cd2d2bc0
taenyen kim <ktn1075@a.ut.ac.kr>
2023-01-22 20:11:50 +0900

    4375 문제풀이

--- algorithm/week1/4375.cpp
+++ algorithm/week1/4375.cpp
@@ -1,8 +1,31 @@
-#include<iostream>
+
+
+#include <iostream>
+#include <cmath>
 
 using namespace std;
 
-int main(void)
-{
+int main() {
 
+    int N;
+    int k = 0;
+    while (cin >> N) {
+        int ans = 1;
+        k = 1;
+
+        while (1) {
+            if (ans % N == 0) {
+                break;
+            }
+            else {
+                k++;
+                ans = ans * 10 + 1;
+                ans %= N;				
+            }
+        }
+
+        cout << k << "\n";
+    }
+
+    return 0;
 }
