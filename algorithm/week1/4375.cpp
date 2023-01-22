f1755712acf3874db3f9f5a870170b6569b8f2b3
taenyen kim <ktn1075@a.ut.ac.kr>
2023-01-22 17:43:09 +0900

    4375 변경

--- algorithm/week1/4375.cpp
+++ algorithm/week1/4375.cpp
@@ -1,53 +1,8 @@
-/*
-#include<stack>
 #include<iostream>
-#include<string>
 
 using namespace std;
 
-bool checkS(string x1)
-{
-	stack<char> gggg;
-	for (auto gg : x1)
-	{
-		if (gggg.empty())
-		{
-			gggg.push(gg);
-			continue;
-		}
-
-		if (gggg.top() == gg)
-		{
-			gggg.pop();
-		}
-		else
-		{
-			gggg.push(gg);
-		}
-	}
-
-	if (gggg.size() ==0)
-	{
-		return true;
-	}
-	return false;
-}
-
 int main(void)
 {
-	int x;
-	string  gg;
-	int count = 0;
-	cin >> x;
 
-	for (int i = 0; i < x; i++)
-	{
-		cin >> gg;
-		
-		if (checkS(gg)) count++;
-	}
-
-	cout << count;
 }
-
-*/
