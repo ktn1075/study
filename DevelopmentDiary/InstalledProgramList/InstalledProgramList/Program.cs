a9b449805b9eb861ce44c0c4a271076b1b6ad540
taenyen kim <ktn1075@a.ut.ac.kr>
2023-03-23 00:01:00 +0900

    INTALL 프로그램 리스트 출력

---
+++ DevelopmentDiary/InstalledProgramList/InstalledProgramList/Program.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace InstalledProgramList
+{
+    internal class Program
+    {
+        static List<string> _installedList = new List<string>();
+
+        static void Main(string[] args)
+        {
+            string path = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall";
+
+            RegistryKey key;
+            key = Registry.CurrentUser.OpenSubKey(path);
+
+            FindProgram(key);
+
+            key = Registry.LocalMachine.OpenSubKey(path);
+
+            FindProgram(key);
+
+            key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall");
+
+            FindProgram(key);
+
+            int countx = 0;
+            foreach (var item in _installedList)
+            {
+                Console.WriteLine($"{countx} : {item}");
+                countx++;   
+            }
+
+            Console.WriteLine(_installedList.Count());   
+        }
+
+        static void FindProgram(RegistryKey key)
+        {
+            foreach (String keyName in key.GetSubKeyNames())
+            {
+                RegistryKey subkey = key.OpenSubKey(keyName);
+                string dp = subkey.GetValue("DisplayName") as string;
+                if (dp != null) 
+                { _installedList.Add(dp); }
+            }
+        }
+    }
+}
