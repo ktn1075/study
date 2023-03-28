963bc2d8f40facc0c255adbc7012fb403abaf935
taenyen kim <ktn1075@a.ut.ac.kr>
2023-03-28 23:50:34 +0900

    동작중인 프로세스 리스트 가져오기

---
+++ DevelopmentDiary/ProcessListGet/ProcessListGet/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessListGet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            getProcess();
+        }
+
+        static void getProcess()
+        {
+            Process[] processes = Process.GetProcesses();
+
+            foreach (Process p in processes)
+            {
+                Console.WriteLine($"PID:{p.Id} - NAME :{p.ProcessName}, SessionId : {p.SessionId}, Memory할당량:{p.WorkingSet64/1000}kb");
+            }
+        }
+    }
+}
