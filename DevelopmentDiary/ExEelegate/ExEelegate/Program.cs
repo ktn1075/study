9c93bf438ae378e802b9d41b752f23aca38632e9
taenyen kim <ktn1075@a.ut.ac.kr>
2023-03-31 00:31:46 +0900

    델리게이트 예제 추가

--- DevelopmentDiary/ExEelegate/ExEelegate/Program.cs
+++ DevelopmentDiary/ExEelegate/ExEelegate/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Reflection.Emit;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +11,39 @@ namespace ExEelegate
 {
     internal class Program
     {
+        public delegate int DETEST (string x);
+
+        public static int test1(string x)
+        {
+            Console.WriteLine("test1");
+            return int.Parse(x)+1;
+        }
+
+        public static int test2(string x)
+        {
+            Console.WriteLine("test2");
+            return int.Parse(x)+2;
+        }
+
         static void Main(string[] args)
         {
+            DETEST dETEST = test1;
+            dETEST += test2;
+
+            int result = dETEST("10");
+
+            Console.WriteLine(result);
+
+            Console.WriteLine("==================================");
+
+            Delegate[] dels = dETEST.GetInvocationList();
+
+            foreach (Delegate d in dels)
+            {
+                DETEST f = (DETEST)d;
+                int ret = f("10");
+                Console.WriteLine(ret);
+            }
         }
     }
 }
