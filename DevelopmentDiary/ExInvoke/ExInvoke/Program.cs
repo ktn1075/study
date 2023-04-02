f11844a543e001f552e436a9bd9b2a3197c0177a
taenyen kim <ktn1075@a.ut.ac.kr>
2023-04-02 21:15:47 +0900

    invoke, beginInvoke 예제

---
+++ DevelopmentDiary/ExInvoke/ExInvoke/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ExInvoke
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string> test = testF;
+            test += testF2;
+
+            test.Invoke("invoke");
+
+            Console.WriteLine($"Invoke:::::Main : {Thread.CurrentThread.ManagedThreadId}");
+
+            // beginInvoke
+
+            Action<string> test11 = testF;
+
+            IAsyncResult result = test11.BeginInvoke("BeginInvoke", null, null);
+
+            Console.WriteLine($"BeginInvoke:::::Main : {Thread.CurrentThread.ManagedThreadId}");
+
+            test11.EndInvoke(result);
+
+        }
+        
+        public static void testF(string x)
+        {
+            Console.WriteLine($"{x}:::::+TestFunc : {Thread.CurrentThread.ManagedThreadId}");
+        }
+
+        public static void testF2(string x)
+        {
+            Console.WriteLine($"{x}:::::TestFunc2 : {Thread.CurrentThread.ManagedThreadId}");
+        }
+    }
+}
