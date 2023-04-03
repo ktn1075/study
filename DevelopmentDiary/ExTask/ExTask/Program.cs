d73dedf5dafd39a5f89024a5b2affeaebb83c8f4
taenyen kim <ktn1075@a.ut.ac.kr>
2023-04-03 22:05:03 +0900

    Task 예제

---
+++ DevelopmentDiary/ExTask/ExTask/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ExTask
+{
+    internal class Program
+    {
+        static void t1()
+        {
+            Console.WriteLine("t1");
+        }
+
+        static void t2()
+        {
+            Thread.Sleep(5000);
+            Console.WriteLine("t2");
+        }
+
+        static void Main(string[] args)
+        {
+            // Action
+            Task task1 = new Task( new Action(t2));
+            task1.Start();
+
+            //델리게이트
+            Task task2 = new Task(delegate { t2(); });
+            task2.Start();
+
+            // 직접 호출
+            Task.Factory.StartNew(t1);
+
+            // 람다
+            Task.Run(() => Console.WriteLine("t3"));
+
+            //task2.Wait();
+            
+            IList<Task> writeTaskList = new List<Task>();
+
+            writeTaskList.Add(task1);
+
+            writeTaskList.Add(task2);
+
+            Task.WhenAll(writeTaskList).Wait();
+            
+        }
+    }
+}
