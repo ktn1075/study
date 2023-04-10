0bd7ebec4b1f94646f673acdca415acb0eee3979
taenyen kim <ktn1075@a.ut.ac.kr>
2023-04-10 23:09:16 +0900

    예제 app coifng 변경

--- DevelopmentDiary/ExNlog/Program.cs
+++ DevelopmentDiary/ExNlog/Program.cs
@@ -2,16 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ExNlog
 {
     internal class Program
     {
+        private static readonly NLog.Logger _logger = NLog.LogManager.GetCurrentClassLogger();
+
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Thread.Sleep(500);
+                _logger.Info("testsdsfafadsfa");
+                _logger.Debug("test defafsdfsfsbug");
+                _logger.Info("testsdsfafadsfa");
+                _logger.Debug("test defafsdfsfsbug");
+                _logger.Info("testsdsfafadsfa");
+                _logger.Debug("test defafsdfsfsbug");
+                _logger.Info("testsdsfafadsfa");
+                _logger.Debug("test defafsdfsfsbug");
+                _logger.Info("testsdsfafadsfa");
+                _logger.Debug("test defafsdfsfsbug");
+                _logger.Info("testsdsfafadsfa");
+                _logger.Debug("test defafsdfsfsbug");
 
-
+            }
         }
     }
 }
